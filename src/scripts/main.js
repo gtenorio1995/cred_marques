f210aa368565dc2a38a89b9343205a696216a0c6
Gabriel Tenório de Oliveira <gabrieltenorio17@gmail.com>
2024-06-10 03:15:10 -0300

    fix: Refinement of the navigation features in the header component, and standardization of the hamburger menu’s animation

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+// MENU BURGUER
+function menuBurguer() {
+  // captura de campos
+  const navigation = document.querySelector("#block_navigation");
+  const btnBurguer = document.querySelector("#menu_burguer");
+  const list_menu = document.querySelectorAll("#block_navigation li");
+
+  const menuToggle = () => {
+    btnBurguer.classList.toggle("active");
+    navigation.classList.toggle("active");
+  };
+
+  btnBurguer.addEventListener("click", menuToggle);
+
+  function removeClass(e) {
+    list_menu.forEach((el) => {
+      el.classList.remove("selected");
+    });
+
+    this.classList.add("selected");
+    menuToggle();
+  }
+
+  list_menu.forEach((item) => {
+    item.addEventListener("click", removeClass);
+  });
+}
+
+menuBurguer();
